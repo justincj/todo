378d41f3f2840e3be9a9492a7c46d4a698afa2c5
justincj <justincgeorge95@gmail.com>
2024-03-19 23:55:31 +0530

    crude version

---
+++ src/lib/components/Timer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher()
+    
+  let countdownTime = 10; // 1 hour in seconds
+
+
+  const countdownInterval = setInterval(() => {
+    countdownTime--;
+    if (countdownTime < 0) {
+      clearInterval(countdownInterval);
+      dispatch('message', null);
+    }
+  }, 1000);
+</script>
+   {Math.floor((countdownTime % 3600) / 60) > 0
+      ? `${Math.floor((countdownTime % 3600) / 60)}:`
+      : ""}
+    {countdownTime % 60 > 0 ? `${countdownTime % 60}`.padStart(2, 0) : ""}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- todo App: 
+form to add to the task.
+each li has a timer which countdown from t-60
+when it reaches zero task is eliminated whether you do it or not.
+-->
+<script>
+    import "$lib/styles/style.css"
+    import Timer from "$lib/components/Timer.svelte"
+    let tasks = [];
+    let inputValue;
+    function handleKeyDown(e){
+        if(e.key=="Enter"){
+            addTask()
+        }
+    }
+
+    function destroy(){
+        console.log('self destruct')
+        tasks = tasks.pop()
+        tasks = tasks;
+    }
+    function addTask(){
+        tasks = [...tasks, inputValue]
+        inputValue=""
+    }
+</script>
+<div class="container">
+    <h1>Self Destructing Todo</h1>
+    <h3>Add the item you want to do for next one hour. it will delte itself after 1 hour</h3>
+    <div>
+        <input on:keydown={handleKeyDown} bind:value={inputValue} placeholder="add task"/>
+        <button on:click={addTask}>submit</button>
+    </div>
+    <ul class="list">
+        {#each tasks as task}
+            <li>
+             <div on:message={destroy} class="wrap">
+                <span>{task}</span>
+                <span on:message><Timer/></span>
+             </div>
+        {/each}
+    </ul>
+</div>
+<style>
+.container {
+	margin: 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #03031c;
+	min-height: 100vh;
+}
+    .wrap {
+        display: flex;
+        gap:2em;
+    }
+    h1, h3, ul {
+        color:white;
+    }
+
+    h1 {
+        margin-bottom: 3px;
+    }
+    h3 {
+        margin-top:0;
+    }
+    .list {
+        list-style: none;
+    }
+</style>
